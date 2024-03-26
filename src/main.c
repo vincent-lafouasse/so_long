2bbab9c2fc82a8b1e461bddacf229977512e57b3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-03-26 14:35:21 +0100

    follow documentation

--- src/main.c
+++ src/main.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "mlx.h"
 
 #define WIDTH 600
 #define HEIGHT 480
 
+typedef struct {
+    void* img;
+    char* addr;
+    int bpp;
+    int line_length;
+    int endian;
+} t_data;
+
+int offset(int x, int y, t_data* data)
+{
+    return y * data->line_length + x * (data->bpp / 8);
+}
+
+void my_mlx_pixel_put(t_data* data, int x, int y,  int color)
+{
+    char* dst = data->addr + offset(x, y, data);
+    *(uint32_t*)dst = color;
+}
+
 int main(void)
 {
     void* mlx = mlx_init();
-    void* mlx_window = mlx_new_window(mlx, WIDTH, HEIGHT, "a window");
 
-    mlx_loop(mlx);
+    t_data img;
+    img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
+    img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_length, &img.endian);
 }
