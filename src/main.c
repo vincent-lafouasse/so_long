e3be9fc931c5b358c8cf69795f277bca13e9a08f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-03-26 13:36:01 +0100

    sketch

--- src/main.c
+++ src/main.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 #include "mlx.h"
 
-#define EXIT_FAILURE 1
-#define EXIT_SUCCESS 0
-
-typedef struct    data_s
-{
-    void          *mlx_ptr;
-    void          *mlx_win;
-}                 data_t;
+#define WIDTH 600
+#define HEIGHT 480
 
 int main(void)
 {
-    data_t        data;
+    void* mlx = mlx_init();
+    void* mlx_window = mlx_new_window(mlx, WIDTH, HEIGHT, "a window");
 
-    if ((data.mlx_ptr = mlx_init()) == NULL)
-        return (EXIT_FAILURE);
-    if ((data.mlx_win = mlx_new_window(data.mlx_ptr, 640, 480, "Hello world")) == NULL)
-        return (EXIT_FAILURE);
-    mlx_loop(data.mlx_ptr);
-    return (EXIT_SUCCESS);
+    mlx_loop(mlx);
 }
