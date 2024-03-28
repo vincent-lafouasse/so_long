782d08abd9fbcc2176050f1618ab03ac351ae5ad
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-03-28 13:28:17 +0100

    rename

--- src/main.c
+++ src/main.c
@@ -20,20 +20,20 @@ typedef struct {
     int bpp;
     int line_length;
     int endian;
-} t_data;
+} t_surface;
 
-void my_mlx_pixel_put(t_data* data, t_position px_pos,  t_u32 color)
+void my_mlx_pixel_put(t_surface* surface, t_position px_pos,  t_u32 color)
 {
-    char* dst = data->addr + flatten_2d_position(px_pos, data->line_length, data->bpp);
+    char* dst = surface->addr + flatten_2d_position(px_pos, surface->line_length, surface->bpp);
     *(t_u32*)dst = color;
 }
 
-void put_rectangle(t_data* data, t_rectangle rect, t_u32 color)
+void put_rectangle(t_surface* surface, t_rectangle rect, t_u32 color)
 {
     for (int col = 0; col < rect.size.w; col++)
     {
         for (int row = 0; row < rect.size.h; row++)
-            my_mlx_pixel_put(data, position(rect.start.x + col, rect.start.y + row),color);
+            my_mlx_pixel_put(surface, position(rect.start.x + col, rect.start.y + row),color);
     }
 }
 
@@ -57,14 +57,14 @@ int main(void)
     vars.mlx = mlx_init();
     vars.window = mlx_new_window(vars.mlx, WIDTH, HEIGHT, WINDOW_NAME);
 
-    t_data img;
-    img.img = mlx_new_image(vars.mlx, WIDTH, HEIGHT);
-    img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_length, &img.endian);
+    t_surface surface;
+    surface.img = mlx_new_image(vars.mlx, WIDTH, HEIGHT);
+    surface.addr = mlx_get_data_addr(surface.img, &surface.bpp, &surface.line_length, &surface.endian);
 
     t_rectangle rect = {{5, 5}, {200, 69}};
-    put_rectangle(&img, rect, CORNFLOWER_BLUE);
+    put_rectangle(&surface, rect, CORNFLOWER_BLUE);
 
-    mlx_put_image_to_window(vars.mlx, vars.window, img.img, 0, 0);
+    mlx_put_image_to_window(vars.mlx, vars.window, surface.img, 0, 0);
 
     mlx_key_hook(vars.window, handle_key_events, &vars);
     mlx_loop(vars.mlx);
