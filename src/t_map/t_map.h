70fdf31ef459c51d5116487c728bc45d9a952796
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-17 15:32:47 +0200

    sketch t_map type

--- src/t_map/t_map.h
+++ src/t_map/t_map.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include "geometry/geometry.h"
+#include "t_list/t_list.h"
 
 typedef struct
 {
 	char** data;
+	t_dimension size;
+	t_position start;
+	t_list* collectibles;
 } t_map;
