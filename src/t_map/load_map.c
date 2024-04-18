0d5651beb6f7f570af83cca0c4031e337005f5b1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-18 15:00:50 +0200

    minor refactor

--- src/t_map/load_map.c
+++ src/t_map/load_map.c
@@ -103,7 +103,10 @@ t_dimension get_map_size(const t_list* map_lst)
 }
 
 char** move_map_to_matrix(t_list** map_lst_ref, t_dimension size);
-void parse_map(t_map* map_return, t_list** map_list_ref);
+void parse_map(t_map* map_return, t_list** map_list_ref)
+{
+    return;
+}
 
 t_map load_map(const char* map_path)
 {
