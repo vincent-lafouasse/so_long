1791336bc18cf7e6056568d63c466e8450ae2e07
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-21 20:27:21 +0200

    implement comparistion functions for geometric types

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -10,6 +10,32 @@ t_dimension dimension(int w, int h)
     return (t_dimension){w, h};
 }
 
+int position_compare(t_position a, t_position b)
+{
+    if (a.x > b.x)
+        return 1;
+    if (a.x < b.x)
+        return -1;
+    if (a.y > b.y)
+        return 1;
+    if (a.y < b.y)
+        return -1;
+    return 0;
+}
+
+int dimension_compare(t_dimension a, t_dimension b)
+{
+    if (a.w > b.w)
+        return 1;
+    if (a.w < b.w)
+        return -1;
+    if (a.h > b.h)
+        return 1;
+    if (a.h < b.h)
+        return -1;
+    return 0;
+}
+
 t_rectangle rectangle(t_position start, t_dimension size)
 {
     return (t_rectangle){start, size};
--- src/geometry/geometry.h
+++ src/geometry/geometry.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdbool.h>
+
 typedef struct
 {
     int x;
@@ -19,7 +21,11 @@ typedef struct
 } t_rectangle;
 
 t_position position(int x, int y);
+int position_compare(t_position a, t_position b);
+
 t_dimension dimension(int w, int h);
+int dimension_compare(t_dimension a, t_dimension b);
+
 t_rectangle rectangle(t_position start, t_dimension size);
 
 int flatten_2d_position(t_position position,
