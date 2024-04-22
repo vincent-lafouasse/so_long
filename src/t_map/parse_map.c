f9d29afde2c16a89c38560cf1fbaaffac256901a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-22 14:54:51 +0200

    sketch parse module and charset struct

--- src/t_map/parse_map.c
+++ src/t_map/parse_map.c
@@ -1,5 +1,21 @@
 #include "t_map_internals.h"
 
+// here ?
+#define VALID_CHARS "01CEP"
+
+struct Charset
+{
+    char WALL;
+    char EMPTY;
+    char PLAYER;
+    char COLLECTIBLE;
+};
+
+static bool map_is_enclosed(const t_map* map);
+static bool map_contains_only_valid_char(const t_map* map);
+static bool map_has_enough_tokens(const t_map* map);
+static bool map_has_valid_path(const t_map* map);
+
 void parse_map(t_map* map)
 {
     (void)map;
