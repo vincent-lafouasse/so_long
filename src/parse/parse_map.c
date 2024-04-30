eea8c1bbff5fb353e0684e756ae9b6fdf045f5f5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-30 15:51:29 +0200

    sketch

--- src/parse/parse_map.c
+++ src/parse/parse_map.c
@@ -1,5 +1,15 @@
 #include "parse_map.h"
 
+// must check for 
+// - enclosed in walls
+// - at least one of each
+// - existence of valid path
+
+static bool map_is_enclosed_in_walls(const t_game* game, t_charset charset);
+static bool map_has_enough_tokens(const t_game* game, t_charset charset);
+static void map_parse_tokens(t_game* t_charset);
+static bool map_has_valid_path(const t_game* game, t_charset charset);
+
 void parse_map(t_game* game, t_charset charset)
 {
     (void)game;
