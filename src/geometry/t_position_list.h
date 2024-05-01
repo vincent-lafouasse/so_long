93812ec2a377226998e24ea1e0a117c1dc3ad2a8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-01 20:00:17 +0200

    sketch position list

---
+++ src/geometry/t_position_list.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "geometry.h"
+
+typedef struct s_position_list
+{
+    t_position position;
+    struct s_position_list* next;
+} t_position_list;
+
+t_position_list* poslst_new(t_position position);
+void poslst_add_front(t_position_list**, t_position_list* node);
+void poslst_remove(t_position_list* poslst_ref, t_position position);
+void poslst_clear(t_position_list** poslst_ref);
