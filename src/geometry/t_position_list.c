4847e0e22fb248dfe46036311e9a1fe4adb5b6a8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-01 21:15:59 +0200

    sketch lstremove

--- src/geometry/t_position_list.c
+++ src/geometry/t_position_list.c
@@ -46,7 +46,28 @@ void poslst_add_front(t_position_list** poslst_ref, t_position_list* node)
 
 void poslst_remove(t_position_list** poslst_ref, t_position position)
 {
+    if (!poslst_ref)
+        return;
 
+    while (position_compare(position, (*poslst_ref)->position) == 0)
+        poslst_delone(poslst_ref);
+
+    if (!*poslst_ref || (*poslst_ref)->next == NULL)
+        return;
+
+    t_position_list* previous = *poslst_ref;
+    t_position_list* current = previous->next;
+
+    while (current)
+    {
+        if (position_compare(position, current->position) == 0)
+        {
+            poslst_delone(&current);
+            previous->next = current;
+        }
+        previous = current;
+        current = current->next;
+    }
 }
 
 void poslst_clear(t_position_list** poslst_ref)
