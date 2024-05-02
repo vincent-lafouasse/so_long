e8fb2c065b62628c5ee678459cc172754799ed4c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-02 15:28:58 +0200

    check if map is enclosed in walls

--- src/parse/parse_map.c
+++ src/parse/parse_map.c
@@ -1,17 +1,36 @@
 #include "parse_map.h"
+#include "error/error.h"
 
 // must check for
 // - enclosed in walls
 // - at least one of each
 // - existence of valid path
 
-static bool map_is_enclosed_in_walls(const t_game* game, t_charset charset);
+static bool map_is_enclosed_in_walls(const t_game* game, t_charset charset)
+{
+    for (int col = 0; col < game->size.w; col++)
+    {
+        if (game->board[0][col] != charset.WALL)
+            return false;
+        if (game->board[game->size.h - 1][col] != charset.WALL)
+            return false;
+    }
+    for (int row = 0; row < game->size.h; row++)
+    {
+        if (game->board[row][0] != charset.WALL)
+            return false;
+        if (game->board[row][game->size.w - 1] != charset.WALL)
+            return false;
+    }
+    return true;
+}
+
 static bool map_has_enough_tokens(const t_game* game, t_charset charset);
 static void map_parse_tokens(t_game* t_charset);
 static bool map_has_valid_path(const t_game* game, t_charset charset);
 
 void parse_map(t_game* game, t_charset charset)
 {
-    (void)game;
-    (void)charset;
+    if (!map_is_enclosed_in_walls(game, charset))
+        free_game(game), die("Map is not enclosed in walls");
 }
