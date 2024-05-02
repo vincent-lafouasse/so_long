3323a3759e02db724525b47e2353d3227ef25816
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-02 15:51:44 +0200

    parse tokens

--- src/parse/parse_map.c
+++ src/parse/parse_map.c
@@ -1,6 +1,7 @@
 #include "parse_map.h"
 #include <stddef.h>
 #include "error/error.h"
+#include "geometry/t_position_list.h"
 #include "parse/t_charset.h"
 
 // must check for
@@ -10,7 +11,7 @@
 
 static bool map_is_enclosed_in_walls(const t_game* game, t_charset charset);
 static bool map_has_enough_tokens(const t_game* game, t_charset charset);
-static void map_parse_tokens(t_game* t_charset);
+static void map_parse_tokens(t_game* game, t_charset charset);
 static bool map_has_valid_path(const t_game* game, t_charset charset);
 
 void parse_map(t_game* game, t_charset charset)
@@ -19,6 +20,9 @@ void parse_map(t_game* game, t_charset charset)
         free_game(game), die("Map is not enclosed in walls");
     if (!map_has_enough_tokens(game, charset))
         free_game(game), die("Invalid map tokens");
+    map_parse_tokens(game, charset);
+    if (position_compare(game->exit, position(0, 0)) == 0)
+        free_game(game), die("Failed to parse map tokens");
 }
 
 static bool map_is_enclosed_in_walls(const t_game* game, t_charset charset)
@@ -60,4 +64,33 @@ static bool map_has_enough_tokens(const t_game* game, t_charset charset)
         }
     }
     return (n_exit == 1) && (n_player == 1) && (n_collectibles >= 1);
+}
+
+static void map_parse_tokens(t_game* game, t_charset charset)
+{
+    char current;
+    t_position_list* node;
+
+    game->collectibles = NULL;
+    for (int row = 0; row < game->size.h; row++)
+    {
+        for (int col = 0; col < game->size.w; col++)
+        {
+            current = game->board[row][col];
+            if (current == charset.EXIT)
+                game->exit = position(row, col);
+            if (current == charset.PLAYER)
+                game->player_position = position(row, col);
+            if (current == charset.COLLECTIBLE)
+            {
+                node = poslst_new(position(row, col));
+                if (!node)
+                {
+                    game->exit = position(0, 0);
+                    return;
+                }
+                poslst_add_front(&(game->collectibles), node);
+            }
+        }
+    }
 }
