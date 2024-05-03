27e41e622ae4fd2266ec231a041bf1856dc244bc
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-03 15:14:49 +0200

    minor refactor

--- src/parse/graph_traversal.c
+++ src/parse/graph_traversal.c
@@ -5,6 +5,48 @@
 
 char** deep_copy_map(const t_game* game);
 bool is_walkable(t_position pos, const t_game* game, t_charset charset);
+t_position_list* find_neighbours(t_position pos, const t_game* game, t_charset charset);
+void set_as_reached(t_position pos, char** reached, t_dimension size);
+bool is_reached(t_position pos, char** reached, t_dimension size);
+
+// a classic bfs
+// marks reached cells with a 'R'
+char** reachable_cells(const t_game* game, t_charset charset)
+{
+    char** reached = deep_copy_map(game);
+    t_position_list* queue = NULL;
+    t_position current;
+    t_position_list* neighbours;
+
+    reached[game->player_position.x][game->player_position.y] = 'R';
+    poslst_emplace_front(&queue, game->player_position);
+
+    while (queue)
+    {
+        current = poslst_pop_back(&queue);
+        neighbours = find_neighbours(current, game, charset);
+        while (neighbours)
+        {
+            if (reached[neighbours->position.x][neighbours->position.y] != 'R')
+            {
+                reached[neighbours->position.x][neighbours->position.y] = 'R';
+            }
+        }
+    }
+
+    return reached;
+}
+
+char** deep_copy_map(const t_game* game)
+{
+    char** copy = malloc(sizeof(char*) * game->size.h);
+
+    for (int row = 0; row < game->size.h; row++)
+    {
+        copy[row] = ft_strdup(game->board[row]);
+    }
+    return copy;
+}
 
 t_position_list* find_neighbours(t_position pos, const t_game* game, t_charset charset)
 {
@@ -45,43 +87,4 @@ bool is_reached(t_position pos, char** reached, t_dimension size)
     if (pos.x >= size.w || pos.x < 0 || pos.y >= size.h || pos.y < 0)
         return false;
     return reached[pos.x][pos.y] == 'R';
-}
-
-// a classic bfs
-// marks reached cells with a 'R'
-char** reachable_cells(const t_game* game, t_charset charset)
-{
-    char** reached = deep_copy_map(game);
-    t_position_list* queue = NULL;
-    t_position current;
-    t_position_list* neighbours;
-
-    reached[game->player_position.x][game->player_position.y] = 'R';
-    poslst_emplace_front(&queue, game->player_position);
-
-    while (queue)
-    {
-        current = poslst_pop_back(&queue);
-        neighbours = find_neighbours(current, game, charset);
-        while (neighbours)
-        {
-            if (reached[neighbours->position.x][neighbours->position.y] != 'R')
-            {
-                reached[neighbours->position.x][neighbours->position.y] = 'R';
-            }
-        }
-    }
-
-    return reached;
-}
-
-char** deep_copy_map(const t_game* game)
-{
-    char** copy = malloc(sizeof(char*) * game->size.h);
-
-    for (int row = 0; row < game->size.h; row++)
-    {
-        copy[row] = ft_strdup(game->board[row]);
-    }
-    return copy;
 }
