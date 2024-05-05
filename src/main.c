5e21c7f9a2023f63aa36330889bac7b21e315580
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-05 15:21:40 +0200

    process keyboard event

--- src/main.c
+++ src/main.c
@@ -24,13 +24,50 @@ typedef struct
     t_mlx* mlx;
 } t_update_input;
 
+typedef enum e_direction
+{
+    Up, Down, Left, Right,
+} t_direction;
+
+static bool is_wall(t_position pos, const t_game* game)
+{
+    return game->board[pos.y][pos.x] == game->charset.WALL;
+}
+
+bool try_move(t_direction direction, t_game* game)
+{
+    t_position new_position = game->player;
+
+    if (direction == Up)
+        new_position.y -= 1;
+    else if (direction == Down)
+        new_position.y += 1;
+    else if (direction == Left)
+        new_position.x -= 1;
+    else if (direction == Right)
+        new_position.x += 1;
+    else
+        return false;
+
+    if (is_wall(new_position, game))
+        return false;
+    game->player = new_position;
+    return true;
+}
+
 int update_game(t_keycode keycode, t_update_input* input)
 {
     log_key_event(keycode);
     if (keycode == XK_Escape)
         mlx_loop_end(input->mlx->mlx);
+    if (keycode == XK_w)
+        try_move(Up, input->game);
+    if (keycode == XK_a)
+        try_move(Left, input->game);
     if (keycode == XK_s)
-        input->game->player.y += 32;
+        try_move(Right, input->game);
+    if (keycode == XK_d)
+        try_move(Down, input->game);
     input->game->needs_render = true;
     return IRRELEVANT_RETURN_VALUE;
 }
