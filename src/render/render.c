81c693ecbb56cc3a7613ec1f081fb6bbcc972cc5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-05-06 14:00:04 +0200

    update dummy ascii render

--- src/render/render.c
+++ src/render/render.c
@@ -22,7 +22,7 @@ int render(t_render_input* params)
     printf("redrawing\n");
     ascii_render(params->game);
     params->game->needs_render = false;
-    printf("window redrawn\n");
+    printf("window redrawn\n\n");
     return IRRELEVANT_RETURN_VALUE;
 }
 
@@ -40,18 +40,24 @@ void ascii_render(const t_game* game)
             t_position current = position(col, row);
             if (is_wall(current, game))
                 printf("#");
-            else if (is_player(current, game) && game->collectibles)
-                printf("x");
-            else if (is_player(current, game) && !game->collectibles)
-                printf("X");
-            else if (is_exit(current, game) && game->collectibles)
-                printf(" ");
-            else if (is_exit(current, game) && !game->collectibles)
-                printf("_");
-            else if (is_collectible(current, game))
-                printf("o");
+            else if (game->collectibles)
+            {
+                if (is_player(current, game))
+                    printf("x");
+                else if (is_collectible(current, game))
+                    printf("o");
+                else 
+                    printf(" ");
+            }
             else
-                printf(" ");
+            {
+                if (is_player(current, game))
+                    printf("X");
+                else if (is_exit(current, game))
+                    printf("*");
+                else 
+                    printf(" ");
+            }
         }
         printf("\n");
     }
