fd70074c45cdc23582c56ab1754a1f1c682022f8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-13 20:24:13 +0200

    format

--- src/game/load/load_raw_map.c
+++ src/game/load/load_raw_map.c
@@ -1,92 +1,112 @@
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_raw_map.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 20:10:23 by poss              #+#    #+#             */
+/*   Updated: 2024/06/13 20:23:52 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "error/error.h"
 #include "get_next_line/get_next_line.h"
 #include "libft/string.h"
 #include "libft/t_list.h"
 #include "load_internals.h"
 #include "t_raw_map.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-static t_list* load_lines_in_lst(const char* map_path);
-static t_raw_map move_str_list_to_map(t_list** str_lst_ref);
-static t_dimension get_map_size(const t_list* map_lst);
+static t_list		*load_lines_in_lst(const char *map_path);
+static t_raw_map	move_str_list_to_map(t_list **str_lst_ref);
+static t_dimension	get_map_size(const t_list *map_lst);
 
-t_raw_map load_raw_map(const char* path)
+t_raw_map	load_raw_map(const char *path)
 {
-    ft_assert(str_ends_with(path, ".ber"),
-              "Invalid map name, map name must end in `.ber`");
-    t_list* lines = load_lines_in_lst(path);
-    ft_assert(lines != NULL, "Failed to read lines from configuration file");
-    t_raw_map map = move_str_list_to_map(&lines);
-    ft_assert(map.data != NULL, "Failed to create char** map");
+	t_list		*lines;
+	t_raw_map	map;
 
-    return map;
+	ft_assert(str_ends_with(path, ".ber"),
+		"Invalid map name, map name must end in `.ber`");
+	lines = load_lines_in_lst(path);
+	ft_assert(lines != NULL, "Failed to read lines from configuration file");
+	map = move_str_list_to_map(&lines);
+	if (map.data == NULL)
+	{
+		ft_lstclear(&lines, &free);
+		die("Failed to create char** map");
+	}
+	return (map);
 }
 
-t_raw_map move_str_list_to_map(t_list** str_lst_ref)
+t_raw_map	move_str_list_to_map(t_list **str_lst_ref)
 {
-    t_raw_map map;
-    t_list** head_ref = str_lst_ref;
-    t_list* current = *head_ref;
-    map.size = get_map_size(*head_ref);
-    if (map.size.h < 2 || map.size.w < 2)
-        ft_lstclear(head_ref, &free), die("map has invalid shape\n");
+	t_raw_map	map;
+	t_list		*current;
+	int			row;
 
-    map.data = malloc(sizeof(char*) * map.size.h);
-    int row = map.size.h - 1;
-    while (row >= 0 && current)
-    {
-        map.data[row] = (current)->content;
-        row--;
-        current = (current)->next;
-    }
-
-    if (row >= 0 || current != NULL)
-        die("mismatched lines and rows");  // might need cleaning up
-
-    ft_lstclear(head_ref, NULL);
-    ft_assert(*head_ref == NULL, "map list was not freed properly.");
-
-    return map;
+	current = *str_lst_ref;
+	map.size = get_map_size(*str_lst_ref);
+	if (map.size.h < 2 || map.size.w < 2)
+	{
+		ft_lstclear(str_lst_ref, &free);
+		die("map has invalid shape\n");
+	}
+	map.data = malloc(sizeof(char *) * map.size.h);
+	row = map.size.h - 1;
+	while (row >= 0 && current)
+	{
+		map.data[row] = (current)->content;
+		row--;
+		current = (current)->next;
+	}
+	if (row >= 0 || current != NULL)
+		die("mismatched lines and rows");
+	ft_lstclear(str_lst_ref, NULL);
+	ft_assert(*str_lst_ref == NULL, "map list was not freed properly.");
+	return (map);
 }
 
-t_list* load_lines_in_lst(const char* map_path)
+t_list	*load_lines_in_lst(const char *map_path)
 {
-    int fd = open(map_path, O_RDONLY);
-    t_list* lines = NULL;
-    t_list* current_node;
-    char* line = get_next_line(fd);
+	int		fd;
+	t_list	*lines;
+	t_list	*current_node;
+	char	*line;
 
-    while (line)
-    {
-        trim_trailing_newline(line);
-        current_node = ft_lstnew(line);
-        ft_lstadd_front(&lines, current_node);
-        line = get_next_line(fd);
-    }
-
-    close(fd);
-    return lines;
+	fd = open(map_path, O_RDONLY);
+	lines = NULL;
+	line = get_next_line(fd);
+	while (line)
+	{
+		trim_trailing_newline(line);
+		current_node = ft_lstnew(line);
+		ft_lstadd_front(&lines, current_node);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (lines);
 }
 
-t_dimension get_map_size(const t_list* map_lst)
+t_dimension	get_map_size(const t_list *map_lst)
 {
-    t_dimension size;
+	t_dimension	size;
 
-    if (!map_lst)
-        return (t_dimension){.w = 0, .h = 0};
-    size.w = ft_strlen(map_lst->content);
-    size.h = 1;
-    map_lst = map_lst->next;
-    while (map_lst)
-    {
-        if (ft_strlen(map_lst->content) != (size_t)size.w)
-            return (t_dimension){.w = 0, .h = 0};
-        size.h++;
-        map_lst = map_lst->next;
-    }
-
-    return size;
+	if (!map_lst)
+		return ((t_dimension){.w = 0, .h = 0});
+	size.w = ft_strlen(map_lst->content);
+	size.h = 1;
+	map_lst = map_lst->next;
+	while (map_lst)
+	{
+		if (ft_strlen(map_lst->content) != (size_t)size.w)
+			return ((t_dimension){.w = 0, .h = 0});
+		size.h++;
+		map_lst = map_lst->next;
+	}
+	return (size);
 }
