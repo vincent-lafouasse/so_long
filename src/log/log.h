c34bdf45aa614e4bd73ea0b0217a5e2cc5936494
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-13 17:42:17 +0200

    format

--- src/log/log.h
+++ src/log/log.h
@@ -1,16 +1,31 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   log.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 17:41:50 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/06/13 17:42:10 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "game/t_game.h"
-#include "libft/t_list.h"
-#include "render/t_mlx.h"
+#ifndef LOG_H
+# define LOG_H
 
-void log_time(void);
-void log_key_event(t_keycode);
-void log_loop_event(void);
+# include "game/t_game.h"
+# include "libft/t_list.h"
+# include "render/t_mlx.h"
 
-void log_str_lst(const t_list* strs);
-void log_char_matrix(const char** matrix, t_dimension size);
-void log_game(t_game game);
-void log_pos(t_position pos);
+void	log_time(void);
+void	log_key_event(t_keycode keycode);
+void	log_loop_event(void);
 
-void ascii_render(const t_game* game);
+void	log_str_lst(const t_list *strs);
+void	log_char_matrix(const char **matrix, t_dimension size);
+void	log_game(t_game game);
+void	log_pos(t_position pos);
+
+void	ascii_render(const t_game *game);
+
+#endif
