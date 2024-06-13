15156b42a9839937244f30670b35d42a03d6807f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-13 20:09:41 +0200

    format

--- src/game/load/load_internals.h
+++ src/game/load/load_internals.h
@@ -1,9 +1,24 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_internals.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 20:09:01 by poss              #+#    #+#             */
+/*   Updated: 2024/06/13 20:09:32 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "t_raw_map.h"
+#ifndef LOAD_INTERNALS_H
+# define LOAD_INTERNALS_H
 
-t_raw_map load_raw_map(const char* path);
+# include "t_raw_map.h"
+
+t_raw_map	load_raw_map(const char *path);
 
 // in string_utils.c
-void trim_trailing_newline(char* s);
-bool str_ends_with(const char* s, const char* pattern);
+void		trim_trailing_newline(char *s);
+bool		str_ends_with(const char *s, const char *pattern);
+
+#endif
