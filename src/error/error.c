fb5675b1a0a4c4220c2ae4ec681a123a1f254497
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-13 17:29:41 +0200

    restructure

--- src/error/error.c
+++ src/error/error.c
@@ -1,3 +1,4 @@
+#include "error.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include "libft/ft_io.h"
@@ -7,4 +8,11 @@ void die(const char* error_message)
     ft_putstr_fd("Error\n", STDERR_FILENO);
     ft_putendl_fd(error_message, STDERR_FILENO);
     exit(1);
+}
+
+void ft_assert(bool assertion, const char* message)
+{
+    if (assertion)
+        return;
+    die(message);
 }
--- src/error/ft_assert.c
+++
@@ -1,8 +0,0 @@
-#include "error.h"
-
-void ft_assert(bool assertion, const char* message)
-{
-    if (assertion)
-        return;
-    die(message);
-}
