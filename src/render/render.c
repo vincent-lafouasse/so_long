eb0bdb1956523d3f41c27c788812a285c220d608
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-13 14:58:00 +0200

    render collectibles

--- src/render/render.c
+++ src/render/render.c
@@ -3,23 +3,37 @@
 #include "image.h"
 #include "render/sprites.h"
 #include "t_mlx.h"
+#include <stdio.h>
 
 #define IRRELEVANT_RETURN_VALUE 0
 
+void render_collectibles(t_render_input* in)
+{
+    for (int row = 0; row < in->game->size.h; row++)
+    {
+        for (int col = 0; col < in->game->size.w; col++)
+        {   if (in->game->board[row][col] == in->game->charset.COLLECTIBLE)
+            render_image(in->mlx, in->sprites->collectible,
+                         position_scale(position(col, row), TILE_SIZE));
+        }
+    }
+
+}
+
 void render_background(t_render_input* in)
 {
-    render_image(in->mlx, &(in->background), position(0, 0));
+    render_image(in->mlx, in->background, position(0, 0));
 }
 
 void render_player(t_render_input* in)
 {
-    render_image(in->mlx, &(in->sprites->player),
+    render_image(in->mlx, in->sprites->player,
                  position_scale(in->game->player, TILE_SIZE));
 }
 
 void render_exit(t_render_input* in)
 {
-    render_image(in->mlx, &(in->sprites->exit),
+    render_image(in->mlx, in->sprites->exit,
                  position_scale(in->game->exit, TILE_SIZE));
 }
 
@@ -27,15 +41,17 @@ int render(t_render_input* params)
 {
     if (!params->needs_refresh)
         return IRRELEVANT_RETURN_VALUE;
+    printf("redrawing\n");
     render_background(params);
+        render_collectibles(params);
     if (params->game->n_collectibles == 0)
         render_exit(params);
     render_player(params);
     return IRRELEVANT_RETURN_VALUE;
 }
 
-void render_image(t_mlx* mlx, const t_image* image, t_position position)
+void render_image(t_mlx* mlx, t_image image, t_position position)
 {
-    mlx_put_image_to_window(mlx->mlx, mlx->window, image->img, position.x,
+    mlx_put_image_to_window(mlx->mlx, mlx->window, image.img, position.x,
                             position.y);
 }
