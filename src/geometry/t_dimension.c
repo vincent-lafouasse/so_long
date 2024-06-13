f401512a21dad203defdf17a52f5a3849e478e7b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-06-13 17:40:45 +0200

    restructure

---
+++ src/geometry/t_dimension.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_dimension.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 17:39:43 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/06/13 17:39:44 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "geometry.h"
+
+t_dimension dimension(int w, int h)
+{
+    return (t_dimension){w, h};
+}
+
+t_dimension dimension_scale(t_dimension dim, size_t scaling_factor)
+{
+    return (t_dimension){.w = dim.w * scaling_factor, .h = dim.h * scaling_factor};
+}
---
+++ src/geometry/t_position.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_position.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 17:39:02 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/06/13 17:39:19 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "geometry.h"
+
+t_position position(int x, int y)
+{
+    return (t_position){x, y};
+}
+
+int position_compare(t_position a, t_position b)
+{
+    if (a.x > b.x)
+        return 1;
+    if (a.x < b.x)
+        return -1;
+    if (a.y > b.y)
+        return 1;
+    if (a.y < b.y)
+        return -1;
+    return 0;
+}
+
+bool position_eq(t_position a, t_position b)
+{
+    return position_compare(a, b) == 0;
+}
+
+t_position position_scale(t_position pos, size_t scaling_factor)
+{
+    return (t_position){.x = pos.x * scaling_factor, .y = pos.y * scaling_factor};
+}
