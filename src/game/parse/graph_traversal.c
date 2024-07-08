99a41356b669496ac8e48fe2851269cdbec10ffd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 13:38:19 +0200

    format

--- src/game/parse/graph_traversal.c
+++ src/game/parse/graph_traversal.c
@@ -11,70 +11,73 @@
 /* ************************************************************************** */
 
 #include "game/load/t_map.h"
-#include "parse_map.h"
 #include "parse_internals.h"
+#include "parse_map.h"
 
-static void flood_fill(char** reached, t_position start, const t_game* game);
-static bool collectibles_are_reached(const char** reached, const t_game* game);
+static void	flood_fill(char **reached, t_position start, const t_game *game);
+static bool	collectibles_are_reached(const char **reached, const t_game *game);
 
-bool has_valid_path(const t_game* game)
+bool	has_valid_path(const t_game *game)
 {
-    char** reached = deep_copy_map(game);
-    flood_fill(reached, game->player, game);
-    if (!is_reached((const char**)reached, game->player, game))
-    {
-        free_map((t_map){reached, game->size});
-        return false;
-    }
-    if (!is_reached((const char**)reached, game->exit, game))
-    {
-        free_map((t_map){reached, game->size});
-        return false;
-    }
-    if (!collectibles_are_reached((const char**)reached, game))
-    {
-        free_map((t_map){reached, game->size});
-        return false;
-    }
-    free_map((t_map){reached, game->size});
-    return true;
+	char	**reached;
+
+	reached = deep_copy_map(game);
+	flood_fill(reached, game->player, game);
+	if (!is_reached((const char **)reached, game->player, game))
+	{
+		free_map((t_map){reached, game->size});
+		return (false);
+	}
+	if (!is_reached((const char **)reached, game->exit, game))
+	{
+		free_map((t_map){reached, game->size});
+		return (false);
+	}
+	if (!collectibles_are_reached((const char **)reached, game))
+	{
+		free_map((t_map){reached, game->size});
+		return (false);
+	}
+	free_map((t_map){reached, game->size});
+	return (true);
 }
 
-void flood_fill(char** reached, t_position start, const t_game* game)
+void	flood_fill(char **reached, t_position start, const t_game *game)
 {
-    if (!is_walkable(start, game))
-    {
-        return;
-    }
-    if (is_reached((const char**)reached, start, game))
-    {
-        return;
-    }
-    reached[start.y][start.x] = 'R';
-    flood_fill(reached, position(start.x + 1, start.y), game);
-    flood_fill(reached, position(start.x - 1, start.y), game);
-    flood_fill(reached, position(start.x, start.y + 1), game);
-    flood_fill(reached, position(start.x, start.y - 1), game);
+	if (!is_walkable(start, game))
+	{
+		return ;
+	}
+	if (is_reached((const char **)reached, start, game))
+	{
+		return ;
+	}
+	reached[start.y][start.x] = 'R';
+	flood_fill(reached, position(start.x + 1, start.y), game);
+	flood_fill(reached, position(start.x - 1, start.y), game);
+	flood_fill(reached, position(start.x, start.y + 1), game);
+	flood_fill(reached, position(start.x, start.y - 1), game);
 }
 
-static bool collectibles_are_reached(const char** reached, const t_game* game)
+static bool	collectibles_are_reached(const char **reached, const t_game *game)
 {
-    int row = 0;
-    int col;
+	int	row;
+	int	col;
 
-    while (row < game->size.h)
-    {
-        col = 0;
-        while (col < game->size.w)
-        {
-            if (game->board[row][col] == game->charset.collectible &&
-                !is_reached(reached, position(col, row), game))
-            {
-                return (false);
-            }
-            col++;
-        }
-        row++;
-    }
-    return true;
+	row = 0;
+	while (row < game->size.h)
+	{
+		col = 0;
+		while (col < game->size.w)
+		{
+			if (game->board[row][col] == game->charset.collectible
+				&& !is_reached(reached, position(col, row), game))
+			{
+				return (false);
+			}
+			col++;
+		}
+		row++;
+	}
+	return (true);
 }
