66ebeac15e33a06779f0433f3d69342f1bd9b45b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 12:26:30 +0200

    format

--- src/render/t_mlx.h
+++ src/render/t_mlx.h
@@ -1,14 +1,29 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_mlx.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:25:37 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 12:26:18 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "geometry/geometry.h"
-#include "mlx.h"
+#ifndef T_MLX_H
+# define T_MLX_H
 
-typedef struct
+# include "geometry/geometry.h"
+# include "mlx.h"
+
+typedef struct s_mlx
 {
-    void* mlx;
-    void* window;
-} t_mlx;
+	void	*mlx;
+	void	*window;
+}			t_mlx;
 
-typedef int t_keycode;
+typedef int	t_keycode;
 
-t_mlx init_mlx(t_dimension window_size, const char* window_name);
+t_mlx		init_mlx(t_dimension window_size, const char *window_name);
+
+#endif
