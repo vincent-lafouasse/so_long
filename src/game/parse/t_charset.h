d2fd6a79f1fc8416c327b2611a6e5334808eca98
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 12:46:50 +0200

    format

--- src/game/parse/t_charset.h
+++ src/game/parse/t_charset.h
@@ -1,17 +1,32 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_charset.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:46:05 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 12:46:20 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdbool.h>
+#ifndef T_CHARSET_H
+# define T_CHARSET_H
 
-#define DEFAULT_CHARSET "01CEP"
+# include <stdbool.h>
+
+# define DEFAULT_CHARSET "01CEP"
 
 typedef struct s_charset
 {
-    char EMPTY;
-    char WALL;
-    char COLLECTIBLE;
-    char EXIT;
-    char PLAYER;
-} t_charset;
+	char	EMPTY;
+	char	WALL;
+	char	COLLECTIBLE;
+	char	EXIT;
+	char	PLAYER;
+}			t_charset;
 
-t_charset default_charset(void);
-bool is_in_charset(char c, t_charset charset);
+t_charset	default_charset(void);
+bool		is_in_charset(char c, t_charset charset);
+
+#endif
