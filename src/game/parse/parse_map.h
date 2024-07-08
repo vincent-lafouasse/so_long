e7daff7681fd9007058d899f7c9637873a11b5cb
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 13:34:34 +0200

    format

--- src/game/parse/parse_map.h
+++ src/game/parse/parse_map.h
@@ -1,6 +1,21 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_map.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 13:33:09 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 13:33:40 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "game/t_game.h"
+#ifndef PARSE_MAP_H
+# define PARSE_MAP_H
 
-void parse_map(t_game* game);
-bool has_valid_path(const t_game* game);
+# include "game/t_game.h"
+
+void	parse_map(t_game *game);
+bool	has_valid_path(const t_game *game);
+
+#endif
--- src/game/parse/t_charset.c
+++ src/game/parse/t_charset.c
@@ -1,20 +1,32 @@
-#include "t_charset.h"
-#include "libft/string.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_charset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 13:33:58 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 13:34:19 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-t_charset default_charset(void)
+#include "libft/string.h"
+#include "t_charset.h"
+
+t_charset	default_charset(void)
 {
-    t_charset charset;
-    charset.empty = '0';
-    charset.wall = '1';
-    charset.collectible = 'C';
-    charset.exit = 'E';
-    charset.player = 'P';
-    return charset;
+	t_charset	charset;
+
+	charset.empty = '0';
+	charset.wall = '1';
+	charset.collectible = 'C';
+	charset.exit = 'E';
+	charset.player = 'P';
+	return (charset);
 }
 
-bool is_in_charset(char c, t_charset charset)
+bool	is_in_charset(char c, t_charset charset)
 {
-    return (charset.wall == c) || (charset.empty == c) ||
-           (charset.player == c) || (charset.exit == c) ||
-           (charset.collectible == c);
+	return ((charset.wall == c) || (charset.empty == c) || (charset.player == c)
+		|| (charset.exit == c) || (charset.collectible == c));
 }
