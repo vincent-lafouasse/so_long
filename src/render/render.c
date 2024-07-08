7339a00e413477da1f5d91fc292b7023d9129068
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 12:19:07 +0200

    format

--- src/render/render.c
+++ src/render/render.c
@@ -1,53 +1,72 @@
-#include "render.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:16:59 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 12:18:13 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "geometry/geometry.h"
 #include "image.h"
+#include "render.h"
 #include "render/sprites.h"
 #include "t_mlx.h"
 
 #define IRRELEVANT_RETURN_VALUE 0
 
-void render_collectibles(t_render_input* in)
+void	render_collectibles(t_render_input *in)
 {
-    for (int row = 0; row < in->game->size.h; row++)
-    {
-        for (int col = 0; col < in->game->size.w; col++)
-        {   if (in->game->board[row][col] == in->game->charset.COLLECTIBLE)
-            render_image(in->mlx, in->sprites->collectible,
-                         position_scale(position(col, row), TILE_SIZE));
-        }
-    }
+	int	row;
+	int	col;
 
+	row = 0;
+	while (row < in->game->size.h)
+	{
+		col = 0;
+		while (col < in->game->size.w)
+		{
+			if (in->game->board[row][col] == in->game->charset.COLLECTIBLE)
+				render_image(in->mlx, in->sprites->collectible,
+					position_scale(position(col, row), TILE_SIZE));
+			col++;
+		}
+		row++;
+	}
 }
 
-void render_background(t_render_input* in)
+void	render_background(t_render_input *in)
 {
-    render_image(in->mlx, in->background, position(0, 0));
+	render_image(in->mlx, in->background, position(0, 0));
 }
 
-void render_player(t_render_input* in)
+void	render_player(t_render_input *in)
 {
-    render_image(in->mlx, in->sprites->player,
-                 position_scale(in->game->player, TILE_SIZE));
+	render_image(in->mlx, in->sprites->player, position_scale(in->game->player,
+			TILE_SIZE));
 }
 
-void render_exit(t_render_input* in)
+void	render_exit(t_render_input *in)
 {
-    render_image(in->mlx, in->sprites->exit,
-                 position_scale(in->game->exit, TILE_SIZE));
+	render_image(in->mlx, in->sprites->exit, position_scale(in->game->exit,
+			TILE_SIZE));
 }
 
-int render(t_render_input* params)
+int	render(t_render_input *params)
 {
-    render_background(params);
-        render_collectibles(params);
-    if (params->game->n_collectibles == 0)
-        render_exit(params);
-    render_player(params);
-    return IRRELEVANT_RETURN_VALUE;
+	render_background(params);
+	render_collectibles(params);
+	if (params->game->n_collectibles == 0)
+		render_exit(params);
+	render_player(params);
+	return (IRRELEVANT_RETURN_VALUE);
 }
 
-void render_image(t_mlx* mlx, t_image image, t_position position)
+void	render_image(t_mlx *mlx, t_image image, t_position position)
 {
-    mlx_put_image_to_window(mlx->mlx, mlx->window, image.img, position.x,
-                            position.y);
+	mlx_put_image_to_window(mlx->mlx, mlx->window, image.img, position.x,
+		position.y);
 }
--- src/render/render.h
+++ src/render/render.h
@@ -1,10 +1,25 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   render.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:16:30 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 12:16:45 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "game/t_game.h"
-#include "image.h"
-#include "sprites.h"
-#include "hooks/hooks.h"
-#include "t_mlx.h"
+#ifndef RENDER_H
+# define RENDER_H
 
-int render(t_render_input* params);
-void render_image(t_mlx* mlx, t_image image, t_position position);
+# include "game/t_game.h"
+# include "hooks/hooks.h"
+# include "image.h"
+# include "sprites.h"
+# include "t_mlx.h"
+
+int		render(t_render_input *params);
+void	render_image(t_mlx *mlx, t_image image, t_position position);
+
+#endif
