d57266a27658a354b130b813fc34f482924cb528
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 12:28:56 +0200

    formaat

--- src/game/t_game.h
+++ src/game/t_game.h
@@ -1,20 +1,35 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_game.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:28:11 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 12:28:41 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdbool.h>
-#include "geometry/geometry.h"
-#include "parse/t_charset.h"
+#ifndef T_GAME_H
+# define T_GAME_H
 
-typedef struct
+# include "geometry/geometry.h"
+# include "parse/t_charset.h"
+# include <stdbool.h>
+
+typedef struct s_game
 {
-    char** board;
-    t_dimension size;
-    t_position player;
-    t_position exit;
-    unsigned int n_collectibles;
-    size_t move_count;
-    t_charset charset;
-    bool done;
-} t_game;
+	char			**board;
+	t_dimension		size;
+	t_position		player;
+	t_position		exit;
+	unsigned int	n_collectibles;
+	size_t			move_count;
+	t_charset		charset;
+	bool			done;
+}					t_game;
 
-t_game init_game(const char* map_path, t_charset charset);
-void free_game(t_game* game);
+t_game				init_game(const char *map_path, t_charset charset);
+void				free_game(t_game *game);
+
+#endif
