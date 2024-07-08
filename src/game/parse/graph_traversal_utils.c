246e465299543e54c9e8e0c369326fc1bff5a94d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 13:36:16 +0200

    format

--- src/game/parse/graph_traversal_utils.c
+++ src/game/parse/graph_traversal_utils.c
@@ -6,36 +6,43 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 13:25:51 by vlafouas          #+#    #+#             */
-/*   Updated: 2024/07/08 13:28:41 by vlafouas         ###   ########.fr       */
+/*   Updated: 2024/07/08 13:35:54 by vlafouas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parse_internals.h"
 #include "libft/string.h"
+#include "parse_internals.h"
 #include <stdlib.h>
 
-bool is_reached(const char** reached, t_position position, const t_game* game)
+bool	is_reached(const char **reached, t_position position,
+		const t_game *game)
 {
-    if (!is_walkable(position, game))
-        return false;
-    return reached[position.y][position.x] == 'R';
+	if (!is_walkable(position, game))
+		return (false);
+	return (reached[position.y][position.x] == 'R');
 }
 
-bool is_walkable(t_position pos, const t_game* game)
+bool	is_walkable(t_position pos, const t_game *game)
 {
-    if (pos.x >= game->size.w || pos.x < 0 || pos.y >= game->size.h ||
-        pos.y < 0)
-        return false;
-    return game->board[pos.y][pos.x] != game->charset.wall;
+	if (pos.x >= game->size.w || pos.x < 0 || pos.y >= game->size.h
+		|| pos.y < 0)
+		return (false);
+	return (game->board[pos.y][pos.x] != game->charset.wall);
 }
 
-char** deep_copy_map(const t_game* game)
+char	**deep_copy_map(const t_game *game)
 {
-    char** copy = malloc(sizeof(char*) * game->size.h);
+	char	**copy;
+	int		row;
 
-    for (int row = 0; row < game->size.h; row++)
-    {
-        copy[row] = ft_strdup(game->board[row]);
-    }
-    return copy;
+	copy = malloc(sizeof(char *) * game->size.h);
+	if (!copy)
+		return (NULL);
+	row = 0;
+	while (row < game->size.h)
+	{
+		copy[row] = ft_strdup(game->board[row]);
+		row++;
+	}
+	return (copy);
 }
