16cc278c1e6f8f0b8946be98a65c13c85a42d449
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 12:42:11 +0200

    format

--- src/game/load/load_internals.h
+++ src/game/load/load_internals.h
@@ -18,7 +18,7 @@
 t_map	load_raw_map(const char *path);
 
 // in string_utils.c
-void		trim_trailing_newline(char *s);
-bool		str_ends_with(const char *s, const char *pattern);
+void	trim_trailing_newline(char *s);
+bool	str_ends_with(const char *s, const char *pattern);
 
 #endif
