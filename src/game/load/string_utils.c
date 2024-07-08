3f388faeaf600f7e2b185c795c0506e2566300a0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 12:44:06 +0200

    format

--- src/game/load/string_utils.c
+++ src/game/load/string_utils.c
@@ -1,25 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   string_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:43:33 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 12:43:47 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/string.h"
 #include "load_internals.h"
 
-void trim_trailing_newline(char* s)
+void	trim_trailing_newline(char *s)
 {
-    size_t len = ft_strlen(s);
+	size_t	len;
 
-    if (len == 0)
-        return;
-
-    if (s[len - 1] == '\n')
-        s[len - 1] = '\0';
+	len = ft_strlen(s);
+	if (len == 0)
+		return ;
+	if (s[len - 1] == '\n')
+		s[len - 1] = '\0';
 }
 
-bool str_ends_with(const char* s, const char* pattern)
+bool	str_ends_with(const char *s, const char *pattern)
 {
-    size_t offset;
+	size_t	offset;
 
-    if (!s || !pattern)
-        return false;
-    if (ft_strlen(s) < ft_strlen(pattern))
-        return false;
-    offset = ft_strlen(s) - ft_strlen(pattern);
-    return 0 == ft_strncmp(pattern, s + offset, ft_strlen(pattern));
+	if (!s || !pattern)
+		return (false);
+	if (ft_strlen(s) < ft_strlen(pattern))
+		return (false);
+	offset = ft_strlen(s) - ft_strlen(pattern);
+	return (0 == ft_strncmp(pattern, s + offset, ft_strlen(pattern)));
 }
