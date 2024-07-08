84a09f0dbe9f70477bd8c20b571efc92dc8a3dd8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 12:27:25 +0200

    format

--- src/render/sprites.c
+++ src/render/sprites.c
@@ -1,5 +1,17 @@
-#include "sprites.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sprites.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:27:11 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 12:27:15 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "render/image.h"
+#include "sprites.h"
 
 #define PLAYER_XPM "./assets/sprites/player.xpm"
 #define COLLECTIBLE_XPM "./assets/sprites/collectible.xpm"
@@ -7,14 +19,14 @@
 #define FLOOR_XPM "./assets/sprites/floor.xpm"
 #define WALL_XPM "./assets/sprites/wall.xpm"
 
-t_sprites load_sprites(t_mlx mlx)
+t_sprites	load_sprites(t_mlx mlx)
 {
-    t_sprites sprites;
+	t_sprites	sprites;
 
-    sprites.player = load_image_xpm(PLAYER_XPM, mlx);
-    sprites.collectible = load_image_xpm(COLLECTIBLE_XPM, mlx);
-    sprites.exit = load_image_xpm(EXIT_XPM, mlx);
-    sprites.floor = load_image_xpm(FLOOR_XPM, mlx);
-    sprites.wall = load_image_xpm(WALL_XPM, mlx);
-    return sprites;
+	sprites.player = load_image_xpm(PLAYER_XPM, mlx);
+	sprites.collectible = load_image_xpm(COLLECTIBLE_XPM, mlx);
+	sprites.exit = load_image_xpm(EXIT_XPM, mlx);
+	sprites.floor = load_image_xpm(FLOOR_XPM, mlx);
+	sprites.wall = load_image_xpm(WALL_XPM, mlx);
+	return (sprites);
 }
--- src/render/sprites.h
+++ src/render/sprites.h
@@ -1,16 +1,31 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sprites.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:26:40 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 12:26:57 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "image.h"
+#ifndef SPRITES_H
+# define SPRITES_H
 
-#define TILE_SIZE 64
+# include "image.h"
+
+# define TILE_SIZE 64
 
 typedef struct s_sprites
 {
-    t_image wall;
-    t_image floor;
-    t_image player;
-    t_image collectible;
-    t_image exit;
-} t_sprites;
+	t_image	wall;
+	t_image	floor;
+	t_image	player;
+	t_image	collectible;
+	t_image	exit;
+}			t_sprites;
 
-t_sprites load_sprites(t_mlx mlx);
+t_sprites	load_sprites(t_mlx mlx);
+
+#endif
