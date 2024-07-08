5a04b06d381d634d2fe535ff09a509194c22f082
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 12:16:17 +0200

    format

--- src/render/image.c
+++ src/render/image.c
@@ -1,20 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   image.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:16:02 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 12:16:06 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "image.h"
 
-t_mlx init_mlx(t_dimension window_size, const char* window_name)
+t_mlx	init_mlx(t_dimension window_size, const char *window_name)
 {
-    t_mlx mlx;
-    mlx.mlx = mlx_init();
-    mlx.window = mlx_new_window(mlx.mlx, window_size.w, window_size.h,
-                                (char*)window_name);
-    return mlx;
+	t_mlx	mlx;
+
+	mlx.mlx = mlx_init();
+	mlx.window = mlx_new_window(mlx.mlx, window_size.w, window_size.h,
+			(char *)window_name);
+	return (mlx);
 }
 
-t_image load_image_xpm(const char* path, t_mlx mlx)
+t_image	load_image_xpm(const char *path, t_mlx mlx)
 {
-    t_image out;
-    out.img =
-        mlx_xpm_file_to_image(mlx.mlx, (char*)path, &out.size.w, &out.size.h);
-    out.addr =
-        mlx_get_data_addr(out.img, &out.bpp, &out.line_length, &out.endianness);
-    return out;
+	t_image	out;
+
+	out.img = mlx_xpm_file_to_image(mlx.mlx, (char *)path, &out.size.w,
+			&out.size.h);
+	out.addr = mlx_get_data_addr(out.img, &out.bpp, &out.line_length,
+			&out.endianness);
+	return (out);
 }
--- src/render/image.h
+++ src/render/image.h
@@ -1,16 +1,31 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   image.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 12:15:02 by vlafouas          #+#    #+#             */
+/*   Updated: 2024/07/08 12:15:55 by vlafouas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "geometry/geometry.h"
-#include "t_mlx.h"
+#ifndef IMAGE_H
+# define IMAGE_H
 
-typedef struct
+# include "geometry/geometry.h"
+# include "t_mlx.h"
+
+typedef struct s_image
 {
-    void* img;
-    char* addr;
-    int bpp;
-    int line_length;
-    int endianness;
-    t_dimension size;
-} t_image;
+	void		*img;
+	char		*addr;
+	int			bpp;
+	int			line_length;
+	int			endianness;
+	t_dimension	size;
+}				t_image;
 
-t_image load_image_xpm(const char* path, t_mlx mlx);
+t_image			load_image_xpm(const char *path, t_mlx mlx);
+
+#endif
