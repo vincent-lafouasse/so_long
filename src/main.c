c83b8032bc6d6a042fbfb3e71ecace664d1f30ff
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-12 03:48:40 +0200

    refactor

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 20:01:01 by poss              #+#    #+#             */
-/*   Updated: 2024/07/12 03:45:37 by poss             ###   ########.fr       */
+/*   Updated: 2024/07/12 03:48:35 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,7 @@ struct s_data {
 	t_image *background;
 };
 
-static void	cleanup_and_exit(t_mlx *mlx, t_sprites *sprites,
-				t_image *background, t_game *game, const char *msg);
-
-static void cleanup_bis(t_data data, const char* msg);
+static void cleanup_and_exit(t_data data, const char* msg);
 
 int	main(int ac, char **av)
 {
@@ -48,10 +45,10 @@ int	main(int ac, char **av)
 	game = init_game(av[1], default_charset());
 	mlx = init_mlx(dimension_scale(game.size, TILE_SIZE), "a cool game");
 	if (!mlx.mlx || !mlx.window)
-		cleanup_bis((t_data){&game, &mlx, NULL, NULL}, "Error building window");
+		cleanup_and_exit((t_data){&game, &mlx, NULL, NULL}, "Error building window");
 	sprites = load_sprites(mlx);
 	if (sprites_are_invalid(sprites))
-		cleanup_and_exit(&mlx, &sprites, NULL, &game, "Error loading sprites");
+		cleanup_and_exit((t_data){&game, &mlx, &sprites, NULL}, "Error loading sprites");
 	render_input = build_render_input(&mlx, &game, &sprites, TILE_SIZE);
 	update_input = (t_update_input){&game, &mlx,
 		.needs_refresh = &render_input.needs_refresh};
@@ -59,34 +56,10 @@ int	main(int ac, char **av)
 	mlx_key_hook(mlx.window, &key_hook, &update_input);
 	mlx_loop_hook(mlx.mlx, &loop_hook, &render_input);
 	mlx_loop(mlx.mlx);
-	cleanup_and_exit(&mlx, &sprites, &render_input.background, &game, NULL);
+	cleanup_and_exit((t_data){&game, &mlx, &sprites, &render_input.background}, NULL);
 }
 
-// rewrite as cleanup_and_exit
-static void	cleanup_and_exit(t_mlx *mlx, t_sprites *sprites,
-		t_image *background, t_game *game, const char *msg)
-{
-	if (!mlx)
-		return ;
-	if (background)
-		mlx_destroy_image(mlx->mlx, background->img);
-	if (sprites)
-		clear_sprites_checked(*sprites, *mlx);
-	if (mlx->window)
-		mlx_destroy_window(mlx->mlx, mlx->window);
-	if (mlx->mlx)
-	{
-		mlx_destroy_display(mlx->mlx);
-		free(mlx->mlx);
-	}
-	if (game)
-		free_game(game);
-	if (msg)
-		die(msg);
-	exit(0);
-}
-
-static void cleanup_bis(t_data data, const char* msg)
+static void cleanup_and_exit(t_data data, const char* msg)
 {
 	if (!data.mlx)
 		return ;
